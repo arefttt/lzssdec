6a54a1ae028921016c239ee57d3c74794ccd53e3
Mohammad Aref Tamanadar (sn1per) <arefttt@programmer.net>
2016-12-24 16:48:33 +0330

    Initial Commit

---
+++ lzssdec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lzssdec
+//
+//  Created by areft on 12/24/16.
+//  Copyright © 2016 Mohammad Aref Tamanadar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
